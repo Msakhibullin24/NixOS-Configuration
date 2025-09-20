21499b905ff60bda7cbb9133a9be6fddf1bfac1e
Msakhibullin24 <msakhibullin24@gmail.com>
2025-09-20 19:36:42 +0300

    nixos is cringe

--- home-manager/modules/vscode.nix
+++ home-manager/modules/vscode.nix
@@ -5,7 +5,6 @@
         extensions = with pkgs.vscode-extensions; [
             rooveterinaryinc.roo-cline
             catppuccin.catppuccin-vsc
-            catppuccin.catppuccin-vsc-icons
             ms-python.python
         ];
     };
